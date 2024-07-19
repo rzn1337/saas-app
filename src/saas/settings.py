fc408e6b393cd428a2d12bb9b71def1d7a2a3ee5
Syed Ali Rizwan <alirizwan2003@gmail.com>
2024-07-19 12:38:26 +0500

    updated build args

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -94,15 +94,15 @@ DATABASES = {
     }
 }
 
-CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
-DATABASE_URL = config("DATABASE_URL", cast=str)
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=300)
+DATABASE_URL = config("DATABASE_URL", default=None)
 
 if DATABASE_URL is not None:
     import dj_database_url
     DATABASES = {
         'default': dj_database_url.config(
             default = DATABASE_URL,
-            conn_max_age = 30,
+            conn_max_age = CONN_MAX_AGE,
             conn_health_checks = True,
         )
     }
